b23f39aa077f815ab4bcfc5fdb0016c229c2e84a
user <rkzhs@naver.com>
2024-03-13 15:34:55 +0900

    오류 수정. Todo rgb2hsv v값

--- ImageProcessing/main.cpp
+++ ImageProcessing/main.cpp
@@ -1,5 +1,7 @@
 #include "include.h"
 
+#include <random>
+
 QuadSet buffer;
 HWND winHandle;
 HDC winCon;
@@ -9,6 +11,14 @@ QuadSet image;
 QuadSet grayImage;
 QuadSet histogramImage;
 QuadSet histogramColor;
+
+const bool ONLY_COLOR = true;
+
+class HSV {
+public:
+	double rgbRed;
+	double rgbBlue, rgbGreen;
+};
 
 using Real = float;
 
@@ -43,11 +53,16 @@ LRESULT CALLBACK winProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 
 		BitBlt(ps.hdc, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, NULL, 0, 0, BLACKNESS);
 
-		StretchBlt(ps.hdc, 0, 0, resizedImageWidth, WINDOW_HEIGHT, image.hdc, 0, 0, image.width, image.height, SRCCOPY);
-		StretchBlt(ps.hdc, resizedImageWidth *1, 0, resizedImageWidth, WINDOW_HEIGHT, grayImage.hdc, 0, 0, image.width, image.height, SRCCOPY);
-		StretchBlt(ps.hdc, resizedImageWidth * 2, 0, resizedImageWidth, WINDOW_HEIGHT, histogramImage.hdc, 0, 0, image.width, image.height, SRCCOPY);
-		StretchBlt(ps.hdc, resizedImageWidth * 3, 0, resizedImageWidth, WINDOW_HEIGHT, histogramColor.hdc, 0, 0, image.width, image.height, SRCCOPY);
-
+		if (!ONLY_COLOR) {
+			StretchBlt(ps.hdc, 0, 0, resizedImageWidth, WINDOW_HEIGHT, image.hdc, 0, 0, image.width, image.height, SRCCOPY);
+			StretchBlt(ps.hdc, resizedImageWidth * 1, 0, resizedImageWidth, WINDOW_HEIGHT, grayImage.hdc, 0, 0, image.width, image.height, SRCCOPY);
+			StretchBlt(ps.hdc, resizedImageWidth * 2, 0, resizedImageWidth, WINDOW_HEIGHT, histogramImage.hdc, 0, 0, image.width, image.height, SRCCOPY);
+			StretchBlt(ps.hdc, resizedImageWidth * 3, 0, resizedImageWidth, WINDOW_HEIGHT, histogramColor.hdc, 0, 0, image.width, image.height, SRCCOPY);
+		}
+		else {
+			BitBlt(ps.hdc, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, image.hdc, 0, 0, SRCCOPY);
+			BitBlt(ps.hdc, image.width, 0, WINDOW_WIDTH, WINDOW_HEIGHT, histogramColor.hdc, 0, 0, SRCCOPY);
+		}
 
 		EndPaint(winHandle, &ps);
 		return 0;
@@ -99,7 +114,7 @@ void CCreateConsole()
 	freopen("CONIN$", "r", stdin);
 }
 
-bool CLoadImage(std::wstring s) {
+bool CLoadImage(std::wstring s, QuadSet& image) {
 
 	Gdiplus::Bitmap* bitmap = Gdiplus::Bitmap::FromFile(s.c_str());
 	if (bitmap == nullptr || Gdiplus::Ok != bitmap->GetLastStatus()) {
@@ -148,53 +163,102 @@ void WorkHistogram(QuadSet& image) {
 	}
 }
 
+void RGBtoHSV(RGBQUAD& rgb, HSV& hsv) {
+	int r = rgb.rgbRed;
+	int g = rgb.rgbGreen;
+	int b = rgb.rgbBlue;
+
+	double rt = r / 255.0;
+	double gt = g / 255.0;
+	double bt = b / 255.0;
+	double cMax = max(rt, max(gt, bt));
+	double cMin = min(rt, min(gt, bt));
+	double delta = cMax - cMin;
+
+	double H, S, V;
+
+	if (r == g && g == b)
+		H = 0;
+	else if (r >= g && r >= b) {
+		H = 60 * ((gt - bt) / delta);
+	}
+	else if (g >= b && g >= r) {
+		H = 60 * ((bt - rt) / delta + 2);
+	}
+	else H = 60 * ((rt - gt) / delta + 4);
+	if (H < 0)
+		H += 360;
+
+	if (r == g && g == b) {
+		S = 0;
+	}
+	else S = delta / cMax;
+
+	hsv.rgbRed = H;
+	hsv.rgbGreen = S;
+	hsv.rgbBlue = max(r, max(g, b));
+}
+
+void HSVtoRGB(HSV& hsv, RGBQUAD& pixel) {
+	double H = hsv.rgbRed/60.;
+	double S = hsv.rgbGreen;
+	double V = hsv.rgbBlue;
+	double ff = H - ((int)H);
+	int p = V * (1.0 - S);
+	int q = V * (1.0 - (S * ff));
+	int t = V * (1.0 - (S * (1.0 - ff)));
+
+	if (S <= 0.00001f) {
+		pixel.rgbBlue = pixel.rgbRed = pixel.rgbGreen = V;
+		return;
+	}
+
+	switch ((int)H) {
+	case 0:
+		pixel.rgbRed = V;
+		pixel.rgbGreen = t;
+		pixel.rgbBlue = p;
+		break;
+	case 1:
+		pixel.rgbRed = q;
+		pixel.rgbGreen = V;
+		pixel.rgbBlue = p;
+		break;
+	case 2:
+		pixel.rgbRed = p;
+		pixel.rgbGreen = V;
+		pixel.rgbBlue = t;
+		break;
+	case 3:
+		pixel.rgbRed = p;
+		pixel.rgbGreen = q;
+		pixel.rgbBlue = V;
+		break;
+	case 4:
+		pixel.rgbRed = t;
+		pixel.rgbGreen = p;
+		pixel.rgbBlue = V;
+		break;
+	default:
+		pixel.rgbRed = V;
+		pixel.rgbGreen = p;
+		pixel.rgbBlue = q;
+		break;
+	}
+}
+
 void WorkColorHistogram(QuadSet& image) {
 	RGBQUAD* quad = image.quad;
-	RGBQUAD* hsv = new RGBQUAD[image.width * image.height];
+	HSV* hsv = new HSV[image.width * image.height];
 	int size = image.width * image.height;
 
-	int pixelCount = 0;
-
-	const int allColors = 101;
+	const int allColors = 256;
 	int count[allColors] = { 0, };
 
 	for (int i = 0; i < size; i++) {
-		int r = quad[i].rgbRed;
-		int g = quad[i].rgbRed;
-		int b = quad[i].rgbRed;
+		RGBtoHSV(quad[i], hsv[i]);
 
-		//RGB TO HSV
-		double rt = r / 255.0;
-		double gt = g / 255.0;
-		double bt = b / 255.0;
-		double cMax = max(r, max(g, b));
-		double cMin = min(r, min(g, b));
-		double delta = cMax - cMin;
-
-		int H, S, V;
-		if (r == g && g == b)
-			H = 0;
-		if (r > g && r > b) {
-			H = 60 * ((gt - bt) / delta);
-		}
-		else if (g > b && g > r) {
-			H = 60 * ((bt - rt) / delta + 2);
-		}
-		else H = 60 * ((rt - gt) / delta + 4);
-		if (H < 0)H += 360;
-
-		if (r == g && g == b && r == 0) {
-			S = 0;
-		}
-		else S = delta*100 / cMax;
-
-		V = cMax;
-
-		hsv[i].rgbRed = H;
-		hsv[i].rgbGreen = S;
-		hsv[i].rgbBlue = V;
-
-		count[S] += 1;
+		count[(int) hsv[i].rgbBlue] += 1;
 	}
 
 	int sum = 0;
@@ -202,33 +266,16 @@ void WorkColorHistogram(QuadSet& image) {
 	for (int i = 0; i < allColors; i++) {
 		sum += count[i];
 		changeTo[i] = allColors * (sum / (double)size);
-		if (changeTo[i] == allColors)
+		if (changeTo[i] >= allColors)
 			changeTo[i] = allColors - 1;
 	}
+
 	for (int i = 0; i < size; i++) {
 		auto& hsvPixel = hsv[i];
-		hsvPixel.rgbBlue = changeTo[hsvPixel.rgbBlue];
-
-		int H = hsvPixel.rgbRed;
-		int S = hsvPixel.rgbGreen;
-		int V = hsvPixel.rgbBlue;
-		int C = V/100.0 * S;
-		int X = C * (1 - abs((H / 60) % 2 - 1));
-		int m = V-C;
-
-		int rgb[3];
-		rgb[(7-H/60) % 3] = X;
-		switch (H / 60) {
-		case 1:case 2:rgb[1] = C; break;
-		case 3:case 4:rgb[2] = C; break;
-		default:rgb[0] = C; break;
-		}
-
-		auto& pixel = quad[i];
-		pixel.rgbRed = (rgb[0] + m) * 255;
-		pixel.rgbGreen = (rgb[1] + m) * 255;
-		pixel.rgbBlue = (rgb[2] + m) * 255;
+		hsvPixel.rgbBlue = changeTo[(int) hsvPixel.rgbBlue];
+		HSVtoRGB(hsvPixel, quad[i]);
 	}
+	delete[] hsv;
 }
 
 void Work() {
@@ -239,12 +286,61 @@ void Work() {
 	WorkHistogram(histogramImage);
 	histogramImage.UpdateBitmap();
 
+	WorkColorHistogram(histogramColor);
+	histogramColor.UpdateBitmap();
+}
 
+int Random()
+{
+	static std::random_device rd;
+	static std::mt19937 gen;
+	static std::uniform_int_distribution<int> distribution;
+	return distribution(gen);
+}
+
+void Test_HsvRgb() {
+	{
+		RGBQUAD rgb;
+		RGBQUAD newRgb;
+		rgb.rgbRed = 29;
+		rgb.rgbGreen = 29;
+		rgb.rgbBlue = 9;
+
+		HSV hsv;
+		RGBtoHSV(rgb, hsv);
+		HSVtoRGB(hsv, newRgb);
+
+		if (rgb.rgbRed != newRgb.rgbRed || rgb.rgbGreen != newRgb.rgbGreen || rgb.rgbBlue != newRgb.rgbBlue) {
+			printf("[%d, %d, %d] ", rgb.rgbRed, rgb.rgbGreen, rgb.rgbBlue);
+			printf("[%lf, %lf, %lf] ", hsv.rgbRed, hsv.rgbGreen, hsv.rgbBlue);
+			printf("[%d, %d, %d] \n", newRgb.rgbRed, newRgb.rgbGreen, newRgb.rgbBlue);
+		}
+	}
+	for (int i = 0; i < 1000; i++) {
+		RGBQUAD rgb;
+		RGBQUAD newRgb;
+		rgb.rgbRed = Random() % 256;
+		rgb.rgbGreen = Random() % 256;
+		rgb.rgbBlue = Random() % 256;
+
+		HSV hsv;
+		RGBtoHSV(rgb, hsv);
+		HSVtoRGB(hsv, newRgb);
+
+		int dif = abs(rgb.rgbRed - newRgb.rgbRed) + abs(rgb.rgbGreen - newRgb.rgbGreen) + abs(rgb.rgbBlue - newRgb.rgbBlue);
+
+		if (dif > 3) {
+			printf("[%d, %d, %d] ", rgb.rgbRed, rgb.rgbGreen, rgb.rgbBlue);
+			printf("[%lf, %lf, %lf] ", hsv.rgbRed, hsv.rgbGreen, hsv.rgbBlue);
+			printf("[%d, %d, %d] \n", newRgb.rgbRed, newRgb.rgbGreen, newRgb.rgbBlue);
+		}
+	}
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int CmdShow)
 {
 	CCreateConsole();
+	Test_HsvRgb();
 
 	WNDCLASSEX winc;
 	instance = hInstance;
@@ -259,14 +355,22 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	ULONG_PTR ptr;
 	Gdiplus::GdiplusStartupInput in;
 	Gdiplus::GdiplusStartup(&ptr, &in, 0);
-	if (!CLoadImage(L"png.png")) {
+	if (!CLoadImage(L"png.png", image)) {
 		MessageBox(NULL, L"Cannot load image", L"error", MB_OK);
 		exit(0);
 	}
 	Work();
 
-	resizedImageWidth = (float)WINDOW_WIDTH / 4;
-	WINDOW_HEIGHT = image.height * resizedImageWidth / (float) image.width;
+	if(ONLY_COLOR){
+		WINDOW_WIDTH = image.width * 2;
+		resizedImageWidth = image.width;
+		WINDOW_HEIGHT = image.height;
+	}
+	else {
+		WINDOW_WIDTH = 1500;
+		resizedImageWidth = (float)WINDOW_WIDTH / 4;
+		WINDOW_HEIGHT = image.height * resizedImageWidth / (float)image.width;
+	}
 
 	winHandle = CreateWindowEx(NULL, CLASSNAME, WINDOWNAME, WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX, 0, 0,
 		WINDOW_WIDTH, WINDOW_HEIGHT, NULL, NULL, hInstance, NULL);
